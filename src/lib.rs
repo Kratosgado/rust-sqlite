f9583d76d49fbc4952d456478f95a966cdc47fa5
Kratosgado <mbeahessilfieprince@gmail.com>
2025-05-25 21:33:55 +0000

    impl: tokenize function

--- src/lib.rs
+++ src/lib.rs
@@ -2,5 +2,6 @@ pub mod cursor;
 pub mod db;
 pub mod dbheader;
 pub mod page;
+pub mod sql;
 
 pub use page::pager;
---
+++ src/sql/tokenizer.rs
@@ -0,0 +1,48 @@
+#[derive(Debug, PartialEq, Eq)]
+pub enum Token {
+    Select,
+    As,
+    From,
+    Star,
+    Comma,
+    SemiColon,
+    Identifier(String),
+}
+
+impl Token {
+    pub fn as_identifier(&self) -> Option<&str> {
+        match self {
+            Token::Identifier(ident) => Some(ident),
+            _ => None,
+        }
+    }
+}
+
+pub fn tokenize(input: &str) -> anyhow::Result<Vec<Token>> {
+    let mut tokens = vec![];
+    let mut chars = input.chars().peekable();
+
+    while let Some(c) = chars.next() {
+        match c {
+            '*' => tokens.push(Token::Star),
+            ',' => tokens.push(Token::Comma),
+            ';' => tokens.push(Token::SemiColon),
+            c if c.is_whitespace() => continue,
+            c if c.is_alphabetic() => {
+                let mut ident = c.to_string().to_lowercase();
+                while let Some(cc) = chars.next_if(|&cc| cc.is_alphanumeric() || cc == '_') {
+                    ident.extend(cc.to_lowercase());
+                }
+
+                match ident.as_str() {
+                    "select" => tokens.push(Token::Select),
+                    "as" => tokens.push(Token::As),
+                    "from" => tokens.push(Token::From),
+                    _ => tokens.push(Token::Identifier(ident)),
+                }
+            }
+            _ => return Err(anyhow::anyhow!("unexpected character: {}", c)),
+        }
+    }
+    Ok(tokens)
+}
